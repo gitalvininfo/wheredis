797c2139859af1d1c42943e232216d6798086687
gitalvininfo <yanson.alvin.info@gmail.com>
2025-07-13 00:11:42 +0800

    Add feature to calculate distance between target and click location | Add path between target and click location

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -21,6 +21,10 @@ const Map = () => {
         'marker'
       )) as google.maps.MarkerLibrary;
 
+      const { spherical } = (await google.maps.importLibrary(
+        'geometry'
+      )) as google.maps.GeometryLibrary;
+
       if (mapRef.current) {
         const map = new Map(mapRef.current, {
           center: { lat: 10.685972772962963, lng: 122.96863457824658 },
@@ -29,13 +33,35 @@ const Map = () => {
         });
 
         // Listen for clicks on the map
-        map.addListener('click', (e: google.maps.MapMouseEvent) => {
-          if (e.latLng) {
-            console.log(e.latLng.toJSON());
+        map.addListener('click', (event: google.maps.MapMouseEvent) => {
+          if (event.latLng) {
+            const userClickLocation = new google.maps.LatLng(event.latLng);
+
             new AdvancedMarkerElement({
-              position: e.latLng,
+              position: userClickLocation,
               map: map,
             });
+
+            const userGuessLatLng = new google.maps.LatLng(userClickLocation);
+            const correctLocation = new google.maps.LatLng(10.2926, 123.9022);
+
+            const distanceInMeters = spherical.computeDistanceBetween(
+              userGuessLatLng,
+              correctLocation
+            );
+
+            console.log(`Distance: ${distanceInMeters} meters`);
+
+            const guessLineCoordinates = [userGuessLatLng, correctLocation];
+            const guessPath = new google.maps.Polyline({
+              path: guessLineCoordinates,
+              geodesic: true,
+              strokeColor: '#FF0000',
+              strokeOpacity: 1.0,
+              strokeWeight: 2,
+            });
+
+            guessPath.setMap(map);
           }
         });
       }
